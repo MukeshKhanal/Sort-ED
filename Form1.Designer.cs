40421ee035ae1f52ffd81a342b4e454bae2a1e39
Mukesh Khanal <officialmukeshkhanal@gmail.com>
2025-02-05 00:39:05 -0500

    Revert "I have changed the color and style of the buttons as well as changed in the method and created universal method to convert the corner to rounded corner"
    This reverts commit ca3ce65248cb2c5ba280b78c27907ccd3f5f8972.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,106 +29,87 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SorterWindow));
-            this.FileBrowser = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.browse = new System.Windows.Forms.Button();
-            this.Fileviewers = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.view = new System.Windows.Forms.Button();
             this.Searchbtn = new System.Windows.Forms.Button();
-            this.Uploaderpanel = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.navPanel = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.sort = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.FileBrowser.SuspendLayout();
-            this.Fileviewers.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.navPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // FileBrowser
+            // panel1
             // 
-            this.FileBrowser.BackColor = System.Drawing.Color.White;
-            this.FileBrowser.Controls.Add(this.label2);
-            this.FileBrowser.Controls.Add(this.label1);
-            this.FileBrowser.Controls.Add(this.browse);
-            this.FileBrowser.Location = new System.Drawing.Point(168, 74);
-            this.FileBrowser.Name = "FileBrowser";
-            this.FileBrowser.Size = new System.Drawing.Size(722, 328);
-            this.FileBrowser.TabIndex = 0;
-            
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.browse);
+            this.panel1.Location = new System.Drawing.Point(168, 74);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(722, 328);
+            this.panel1.TabIndex = 0;
             // 
             // browse
             // 
-            this.browse.BackColor = System.Drawing.Color.DeepSkyBlue;
-            this.browse.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.browse.FlatAppearance.BorderSize = 0;
-            this.browse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.browse.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.browse.ForeColor = System.Drawing.Color.White;
-            this.browse.Location = new System.Drawing.Point(252, 235);
+            this.browse.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.browse.Location = new System.Drawing.Point(255, 220);
             this.browse.Name = "browse";
-            this.browse.Size = new System.Drawing.Size(222, 52);
+            this.browse.Size = new System.Drawing.Size(166, 45);
             this.browse.TabIndex = 0;
             this.browse.Text = "Browse Files";
-            this.browse.UseVisualStyleBackColor = false;
+            this.browse.UseVisualStyleBackColor = true;
             // 
-            // Fileviewers
+            // panel2
             // 
-            this.Fileviewers.BackColor = System.Drawing.Color.White;
-            this.Fileviewers.Controls.Add(this.view);
-            this.Fileviewers.Controls.Add(this.Searchbtn);
-            this.Fileviewers.Location = new System.Drawing.Point(925, 48);
-            this.Fileviewers.Name = "Fileviewers";
-            this.Fileviewers.Size = new System.Drawing.Size(279, 678);
-            this.Fileviewers.TabIndex = 1;
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.view);
+            this.panel2.Controls.Add(this.Searchbtn);
+            this.panel2.Location = new System.Drawing.Point(925, 48);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(303, 661);
+            this.panel2.TabIndex = 1;
             // 
             // view
             // 
-            this.view.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.view.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.view.FlatAppearance.BorderSize = 0;
-            this.view.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.view.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.view.ForeColor = System.Drawing.Color.White;
-            this.view.Location = new System.Drawing.Point(70, 601);
+            this.view.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.view.Location = new System.Drawing.Point(88, 600);
             this.view.Name = "view";
             this.view.Size = new System.Drawing.Size(147, 49);
             this.view.TabIndex = 4;
             this.view.Text = "View More";
-            this.view.UseVisualStyleBackColor = false;
+            this.view.UseVisualStyleBackColor = true;
             // 
             // Searchbtn
             // 
-            this.Searchbtn.BackColor = System.Drawing.Color.Gold;
-            this.Searchbtn.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Searchbtn.FlatAppearance.BorderSize = 0;
-            this.Searchbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Searchbtn.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Searchbtn.ForeColor = System.Drawing.Color.White;
-            this.Searchbtn.Location = new System.Drawing.Point(70, 152);
+            this.Searchbtn.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Searchbtn.Location = new System.Drawing.Point(59, 93);
             this.Searchbtn.Name = "Searchbtn";
             this.Searchbtn.Size = new System.Drawing.Size(158, 44);
             this.Searchbtn.TabIndex = 3;
             this.Searchbtn.Text = "Search";
-            this.Searchbtn.UseVisualStyleBackColor = false;
+            this.Searchbtn.UseVisualStyleBackColor = true;
             // 
-            // Uploaderpanel
+            // panel3
             // 
-            this.Uploaderpanel.BackColor = System.Drawing.Color.White;
-            this.Uploaderpanel.Location = new System.Drawing.Point(178, 463);
-            this.Uploaderpanel.Name = "Uploaderpanel";
-            this.Uploaderpanel.Size = new System.Drawing.Size(712, 275);
-            this.Uploaderpanel.TabIndex = 1;
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(168, 463);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(719, 290);
+            this.panel3.TabIndex = 1;
             // 
             // navPanel
             // 
             this.navPanel.BackColor = System.Drawing.Color.White;
             this.navPanel.Controls.Add(this.pictureBox1);
-            this.navPanel.Location = new System.Drawing.Point(12, 48);
+            this.navPanel.Location = new System.Drawing.Point(12, 34);
             this.navPanel.Name = "navPanel";
             this.navPanel.Size = new System.Drawing.Size(141, 609);
             this.navPanel.TabIndex = 2;
+            this.navPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.navPanel_Paint);
             // 
             // pictureBox1
             // 
@@ -142,60 +123,32 @@
             // 
             // sort
             // 
-            this.sort.BackColor = System.Drawing.Color.SteelBlue;
-            this.sort.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.sort.FlatAppearance.BorderSize = 0;
-            this.sort.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.sort.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.sort.ForeColor = System.Drawing.Color.White;
-            this.sort.Location = new System.Drawing.Point(458, 408);
+            this.sort.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sort.Location = new System.Drawing.Point(452, 408);
             this.sort.Name = "sort";
-            this.sort.Size = new System.Drawing.Size(152, 49);
+            this.sort.Size = new System.Drawing.Size(109, 49);
             this.sort.TabIndex = 1;
             this.sort.Text = "Sort It";
-            this.sort.UseVisualStyleBackColor = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label1.Location = new System.Drawing.Point(62, 49);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(602, 60);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Drag and Drop your files here ";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label2.Location = new System.Drawing.Point(309, 143);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(84, 60);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Or:";
+            this.sort.UseVisualStyleBackColor = true;
             // 
             // SorterWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1240, 765);
             this.Controls.Add(this.sort);
             this.Controls.Add(this.navPanel);
-            this.Controls.Add(this.Uploaderpanel);
-            this.Controls.Add(this.Fileviewers);
-            this.Controls.Add(this.FileBrowser);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "SorterWindow";
             this.Text = "SortED";
-            this.FileBrowser.ResumeLayout(false);
-            this.FileBrowser.PerformLayout();
-            this.Fileviewers.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             this.navPanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -204,17 +157,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Panel FileBrowser;
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button browse;
-        private System.Windows.Forms.Panel Fileviewers;
+        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button view;
         private System.Windows.Forms.Button Searchbtn;
-        private System.Windows.Forms.Panel Uploaderpanel;
+        private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel navPanel;
         private System.Windows.Forms.Button sort;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,32 +16,24 @@ namespace SortED
         public SorterWindow()
         {
             InitializeComponent();
-            ApplyRoundedCorners(navPanel, 25);
-            ApplyRoundedCorners(browse, 45);
-            ApplyRoundedCorners(sort, 45);
-            ApplyRoundedCorners(view, 45);
-            ApplyRoundedCorners(Searchbtn, 45);
-            ApplyRoundedCorners(Uploaderpanel, 25);
-            ApplyRoundedCorners(Fileviewers, 25);
+        }
+
+        private void navPanel_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            Rectangle rect = new Rectangle(0, 0, navPanel.Width, navPanel.Height);
+            int radius = 25;
+            GraphicsPath path = new GraphicsPath();
+
+            path.AddArc(0, 0, radius, radius, 180, 90);
+            path.AddArc(rect.Width - radius, 0, radius, radius, 270, 90);
+            path.AddArc(rect.Width - radius, rect.Height - radius, radius, radius, 0, 90);
+            path.AddArc(0, rect.Height - radius, radius, radius, 90, 90);
+            path.CloseFigure();
+           navPanel.Region = new Region(path);
+
+           
            
         }
-
-        //this is the custom method which will be used to round corner in any controls in the given form
-        private void ApplyRoundedCorners(Control control, int radius)
-        {
-            GraphicsPath path = new GraphicsPath();
-            
-            Rectangle rect = new Rectangle(0, 0, control.Width, control.Height);
-
-            path.AddArc(rect.X, rect.Y, radius, radius, 180, 90); // Top-left
-            path.AddArc(rect.Right - radius, rect.Y, radius, radius, 270, 90); // Top-right
-            path.AddArc(rect.Right - radius, rect.Bottom - radius, radius, radius, 0, 90); // Bottom-right
-            path.AddArc(rect.X, rect.Bottom - radius, radius, radius, 90, 90); // Bottom-left
-            path.CloseFigure();
-
-            control.Region = new Region(path);
-        }
-
-       
     }
 }
