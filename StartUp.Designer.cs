32061bb0055140cf0373b34df7c071cabedfc968
Ishan Dahal <dahalishan640@gmail.com>
2025-02-05 20:58:10 -0500

    Made a startUp page and setup loading screen and did changes on UI (background)

--- StartUp.Designer.cs
+++ StartUp.Designer.cs
@@ -28,21 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartUp));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Image = global::SortED.Properties.Resources._3_removebg_preview;
+            this.pictureBox1.Location = new System.Drawing.Point(406, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(427, 491);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(365, 488);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(506, 43);
+            this.progressBar1.TabIndex = 1;
             // 
             // StartUp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SortED.Properties.Resources.bckg;
             this.ClientSize = new System.Drawing.Size(1240, 765);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "StartUp";
             this.Text = "StartUp";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- StartUp.cs
+++ StartUp.cs
@@ -15,6 +15,28 @@ namespace SortED
         public StartUp()
         {
             InitializeComponent();
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+            progressBar1.Value = 0;
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
         }
-    }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value < progressBar1.Maximum)
+            {
+                progressBar1.Value += 2; // Adjust speed
+
+            }
+            else
+            {
+                timer1.Stop();
+                Form Form1 = new SorterWindow();
+                Form1.Show();
+                this.Hide(); // Hide startup form
+            }
+        }
+        }
+
+
 }
