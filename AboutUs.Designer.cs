ce5c70a4ae4e6835df6438f6af17b4be5ae56e35
Ishan Dahal <dahalishan640@gmail.com>
2025-02-25 16:31:50 -0500

    Updated ico files for all the pages

--- AboutUs.Designer.cs
+++ AboutUs.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutUs));
             this.label1 = new System.Windows.Forms.Label();
             this.WhoWeAre = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -187,6 +188,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.WhoWeAre);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "AboutUs";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
--- Help.Designer.cs
+++ Help.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Help));
             this.Title = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -116,6 +117,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Title);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Help";
             this.Text = "Help";
